a002f253e7b17a3e07e5e82979a00feeb84fef93
Damir <damiratamuratov07@gmail.com>
2026-08-05 16:14:23 +0500

    style(ui): install local fonts and add a new variant for the Button component

--- apps/web/src/app/main.tsx
+++ apps/web/src/app/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
+import '@fontsource-variable/rubik'
 import './globals.css'
 import { StrictMode } from 'react'
 
